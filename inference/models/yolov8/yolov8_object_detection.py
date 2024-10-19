a35bbc9fe642b227a24757252e6baccb5139f8a0
Tim Hodgson <timhodgson@protonmail.com>
2024-10-18 23:49:27 -0400

    - update onnx log lever

--- inference/models/yolov8/yolov8_object_detection.py
+++ inference/models/yolov8/yolov8_object_detection.py
@@ -41,8 +41,10 @@ class YOLOv8ObjectDetection(ObjectDetectionBaseOnnxRoboflowInferenceModel):
             Tuple[np.ndarray]: NumPy array representing the predictions, including boxes, confidence scores, and class confidence scores.
         """
         img_in = np.array(img_in, dtype=np.float32)
+        self.onnx_session.set_default_logger_severity(0)
         img_in = onnxruntime.OrtValue.ortvalue_from_numpy(img_in, 'cuda', 0)
         predictions = self.onnx_session.run(None, {self.input_name: img_in})[0]
+        
         print("\nCUDA Usage:")
         if 'CUDAExecutionProvider' in self.onnx_session.get_providers():
             logger.debug("CUDA is being used for inference.")
